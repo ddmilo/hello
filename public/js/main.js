ab70a3b63b82234e15c661ccde5304cacd256394
ddmilo <ddmilo1029@gmail.com>
2017-07-29 15:46:32 -0400

    redid anti-mobile query, window.width wasnt working so I used screen.width

--- public/js/main.js
+++ public/js/main.js
@@ -46,7 +46,8 @@
     }
   });
 
-  if ($(window).width() < 960) {
+
+  if (screen.width <= 960) {
    $('h1').html('Sorry, but currently there is no working mobile version for this app. <br><br>Please use a desktop/laptop and a pair of headphones to fully experience this app. <br><br> Thank You.');
    $('form').hide();
 }
